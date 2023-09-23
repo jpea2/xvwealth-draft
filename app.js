ae697f2661d9d6ae8c36a6015f776b25cef489d8
Jay <124354310+jpea2@users.noreply.github.com>
2023-09-23 15:21:28 +1000

    added ui changes around 'main' div and holdings for crypto

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-let xrp = {
-    ticker: ["XRP"],
-    price: 0.55,
-    holdings: 500,
-};
+let coins = [
+    {
+        ticker: "XRP",
+        price: 0.55,
+        holdings: 500
+    },
+    {
+        ticker: "ETH",
+        price: 7.5,
+        holdings: 20
+    },
+    {
+        ticker: "BTC",
+        price: 20000,
+        holdings: 2
+    }
+];
 
-let ethereum = {
-    ticker: ["ETH"],
-    price: 7.5,
-    holdings: 2000,
-};
 
-let bitcoin = {
-    coins: ["BTC"],
-    price: 20000,
-    holdings: 2,
-};
+let ticker = document.querySelector("#ticker");
+let price = document.querySelector("#price");
+let holdings = document.querySelector("#holdings");
 
-
-let listText = document.querySelector("#listOfCoins");
-
-for (let i = 0; i < list.coins.length; i++) {
-    listText.innerHTML += `<li>${
-        coins + price + holdings
-    }</li>`;
+for (let i = 0; i < coins.length; i++) {
+    ticker.innerHTML += `<li>${coins[i].ticker}</li>`;
+    price.innerHTML += `<li>${coins[i].price}</li>`;
+    holdings.innerHTML += `<li>${coins[i].holdings}</li>`;
 }
